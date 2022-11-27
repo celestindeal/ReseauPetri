612c27bd3ee96f1976f44aeb8762a87cf0508ec2
celestindeal <celestindeal42@gmail.com>
2022-11-27 13:18:09 +0100

    fix : transition

--- TP8_Réseaux/reseauPetri/perti.py
+++ TP8_Réseaux/reseauPetri/perti.py
@@ -1,7 +1,18 @@
 
 
+class Transition():
 
-from transition import Transition
+    def __init__(self, nom, condition, action) -> None:
+        self._nom = nom
+        self._conditions = condition
+        self._actions = action
+        pass
+
+    def addCondition(self, place, jetons):
+        self._conditions[place] = jetons
+
+    def addAction(self, place, jetons):
+        self._actions[place] = jetons
 
 
 class Petri():
