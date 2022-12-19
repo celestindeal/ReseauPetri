1e324a05df3eedfb2fe7d7f059fb39284a58db06
Celestindeal <celestindeal42@gmail.com>
2022-12-19 22:24:48 +0100

    feat: if finding the last node is a loop

---
+++ TP9_Réseaux/reseauPetri/noeud.py
@@ -0,0 +1,70 @@
+
+class Noeud():
+    """
+    noeud d'un arbre
+    """
+
+    def __init__(self, parent):
+        """
+        constructeur
+        """
+        self._valeurs = {}          # nom de la place : nombre de jetons
+        self._parent = parent       # noeud parent
+        self._enfants = {}          # neurds enfants -> clé = nom de la transition : valeur = neoud enfant
+    
+    def toString(self):
+        """
+        retourne la valeur du noeud en str
+        """
+        texte = ""
+        if self._valeurs == {}:
+            texte = "ø"
+        else:
+            for key in self._valeurs:
+                texte += key \
+                    + "^" \
+                    + str(self._valeurs[key]) \
+                    + " "
+        return texte
+    
+    def addValue(self, nomPlace, nbJetons):
+        """
+        compose la valeur du noeud, l'état du marquage en ajoutant pour cette place son nombre de jetons
+        """
+        self._valeurs[nomPlace] = nbJetons
+    
+    def addEnfant(self, nomTransition, neudEnfant):
+        self._enfants[nomTransition] = neudEnfant
+
+    def compareNoeud(self, noeud):
+        """
+        compare si ce neoud a la meme valeur que le noeud en paramètre, cad qu'il a les memes places avec le meme nombre de jetons
+        """
+        for key in self._valeurs:
+            # si la place n'est pas dans les places de l'autre noeud
+            if key not in noeud._valeurs:
+                return False
+            else:
+                # si les deux places existent, on vérifie qu'il y a ait le meme nombre de jetons
+                if self._valeurs[key] != noeud._valeurs[key]:
+                    return False
+
+        # si des places existent dans noeud mais pas dans self -> false
+        for key in noeud._valeurs:
+            if key not in self._valeurs:
+                return False
+
+        return True
+    
+    def compareNoeudGenerator(self, noeudenfant):
+        # pour chaque place dans le noeud en controle que le noeud enfant a la meme place et le meme nombre de jetons ou plus 
+
+        #  problème il existe le cas oui le parent a des place que l'enfant n'a pas
+        for key in noeudenfant._valeurs:
+            try:                    #c'est pas la bonne solution   je veux dire si la place n'existe pas dans le noeud enfant
+                if self._valeurs[key] > noeudenfant._valeurs[key]:
+                    return False
+            except KeyError:
+                return False
+
+        return True
---
+++ TP9_Réseaux/reseauPetri/perti.py
@@ -0,0 +1,114 @@
+from .arbre import Arbre
+from .noeud import Noeud
+from .transition import Transition
+
+
+class Petri():
+
+    def __init__(self, nom ):
+        """
+        constructeur
+        """
+        self._nom = nom
+        self._listPlaces = []
+        self._listTransitions = []
+        self._arbre = Arbre(nom)
+        pass
+
+    def printreseauPetri(self):
+        print("reseauPetri: " + self._nom)
+        print("Places:")
+        for place in self._listPlaces:
+            print(place._nom, ":", place._jetons)
+        print("Transitions:")
+        for transition in self._listTransitions:
+            print(transition._nom)
+            print("\t"+"Conditions:")
+            for place in transition._conditions:
+                print("\t"+place._nom + ": " + str(transition._conditions[place]))
+            print("\t"+"Actions:")
+            for place in transition._actions:
+                print("\t"+place._nom + ": " + str(transition._actions[place]))
+        pass
+
+    def addPlace(self,place):
+        self._listPlaces.append(place)
+
+    def addTransition(self, nom, condition, action):
+        
+        for place in condition:
+            if place not in self._listPlaces:
+                return False
+        for place in action:
+            if place not in self._listPlaces:
+                return False
+        self._listTransitions.append(Transition(nom, condition, action))
+    
+
+    def findJetons(self):
+        placesAvecJetons = []
+        for place in self._listPlaces:
+            if place._jetons > 0:
+                placesAvecJetons.append(place)
+        return placesAvecJetons
+
+
+    def findTransitionsPossibles(self):
+        transitionsPossibles = []
+        for t in self._listTransitions:
+            check = True
+            for key in t._conditions:
+                if key._jetons < t._conditions[key]:
+                    check = False
+            if check:
+                transitionsPossibles.append(t)
+        return transitionsPossibles
+
+    def buildArbre(self):
+        # init
+        marquageInit = self.findJetons()
+        n = Noeud(None)
+        for place in marquageInit:
+            n.addValue(place._nom, place._jetons)
+        self._arbre.addNoeud(n, None)
+
+        self.parcoursArbre(self._arbre._listNoeud[0], 1)
+
+        
+
+    
+    def parcoursArbre(self, antecedent, nb):
+
+        tp = self.findTransitionsPossibles()
+
+        tp_str = ""
+        for t in tp:
+            tp_str += t._nom
+
+        # parcours des transitions possibles
+        for t in tp:
+
+            # execution de la transition
+            t.execTransition()
+
+            # créé un nouveau noeud avec le nouveau marquage
+            marquage = self.findJetons()
+            n = Noeud(antecedent)
+            for place in marquage:
+                n.addValue(place._nom, place._jetons)
+
+            # ajoute le nouveau noeud à l'arbre
+            if not self._arbre.addNoeud(n, t) and not self._arbre.isGenerator(n, t):
+                # recursivite si pas d'antecedent pareil
+                self.parcoursArbre(n, nb+1)
+
+            t.inverserTransition()
+    
+    def printArbre(self):
+        print("\nArbre:", self._arbre._nom)
+        self._arbre.printArbre()
+
+
+
+    def IsReseauBorne(self):
+        return self._arbre._isBorne
